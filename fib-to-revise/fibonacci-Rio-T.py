b0b0602418455a5c1485e6495a99906edf47fe45
tomoka-y <43874245+tomoka-y@users.noreply.github.com>
2018-10-19 17:48:34 +0900

    10/19 フィボナッチです

--- fib-to-revise/fibonacci-Rio-T.py
+++ fib-to-revise/fibonacci-Rio-T.py
@@ -7,11 +7,15 @@ Spyderエディタ
 def fib(n):
 #フィボナッチ数列を定義する。
     a, b = 0, 1
-    for i in range(n):
+    for i in range(1,n-1):
         a, b = b, a + b
     return b
 
 print([fib(i) for i in range(10)]) 
+#リスト内表記
+
+
+    
 #n=1,2,3...,10の時のフィボナッチ数列
 # [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 print(fib(2018)) 
